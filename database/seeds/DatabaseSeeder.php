da78b5364edf604f03a8b5c5190e43695ef8dcfc
Juan Pablo Santos <jsantos@stackbuilders.com>
2015-10-14 10:03:37 -0500

    Add seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(TicketSeeder::class);
 
         Model::reguard();
     }
